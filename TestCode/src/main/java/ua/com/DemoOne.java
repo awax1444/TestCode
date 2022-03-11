72c9aeefa080f81e9d1af3d9c4e7420b467406dd
awax1444 <101391060+awax1444@users.noreply.github.com>
2022-03-11 19:22:02 +0200

    Update DemoOne.java

--- TestCode/src/main/java/ua/com/DemoOne.java
+++ TestCode/src/main/java/ua/com/DemoOne.java
@@ -3,5 +3,6 @@ package ua.com;
 public class DemoOne {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("I am from Ukraine");
     }
 }
